c59e7492c35156821780128950ff67c37e754064
DESKTOP-2O6FVIG\matei <matei.madalin1@gmail.com>
2018-04-19 12:23:36 +0200

    blabla

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ class Hello {
 
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null, "Muie Barca");
+        JOptionPane.showMessageDialog(null, "Hello Everyone);
 
     }
 
