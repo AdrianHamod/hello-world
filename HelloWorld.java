60e71c2c57cf6ccd94252f06569212e97ae27d56
usuario_local <usuario_local@PTO0910>
2018-04-19 12:44:23 +0200

    hey merge conflict solved

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,8 @@ class Hello {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        JOptionPane.showMessageDialog(null, "Hello Everyone);
-=======
         JOptionPane.showMessageDialog(null, "Hello world2");
->>>>>>> refs/heads/feature
+
 
     }
 
